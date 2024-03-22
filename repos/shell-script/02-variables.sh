14f08c659d0b51d77e0e83866293a84cad68f18f
KotiYadav <koti0120@gmail.com>
2024-03-22 21:47:05 +0530

    adding 2nd file

---
+++ repos/shell-script/02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "sailu: hi suji good morning"
+echo "suji: hi sailu very good morning"
+echo "sailu: ho are you suji..?"
+echo "suji: i'm fine sailu what about you..?"
+echo "sailu: i'm also good suji what else..?"
+echo "suji:nothing sailu"
