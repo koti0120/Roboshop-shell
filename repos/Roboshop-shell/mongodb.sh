9fdc8103a535b7a046a3aeca52b5cc6c21bb93e4
KotiYadav <koti0120@gmail.com>
2024-03-30 11:41:57 +0530

    modifying

--- repos/Roboshop-shell/mongodb.sh
+++ repos/Roboshop-shell/mongodb.sh
@@ -8,9 +8,9 @@ Y="\e[33m"
 N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
-LOGFILE="/tmp/$0-$TIMESTAMP.log
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo " Script started executing at $TIMESTAMP " &>> LOGFILE
+echo " Script started executing at $TIMESTAMP " &>> $LOGFILE
 
 VALIDATE() {
     if [ $1 -ne 0 ]
@@ -31,16 +31,25 @@ echo " $G This is root user $N "
 fi
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
 VALIDATE $? "COPIED MONGO REPO"
+
 dnf install mongodb-org -y &>> $LOGFILE
+
 VALIDATE $? "installing mongodb"
+
 systemctl enable mongod &>> $LOGFILE
+
 VALIDATE $? "Enable the mongodb"
+
 systemctl start mongod &>> $LOGFILE
+
 VALIDATE $? "start the mongodb"
 
 sed -i '/s/127.0.0.1 to 0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
 VALIDATE $? "remote access for mongodb"
 
 systemctl restart mongod &>> $LOGFILE
+
 VALIDATE $? "restarting the mongodb"
